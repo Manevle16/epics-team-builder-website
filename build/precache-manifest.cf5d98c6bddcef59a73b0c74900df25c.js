602698acec954e1da9b17542042c596a12746ad5
Matthew Nevle <Manevle16@gmail.com>
2020-02-20 14:26:53 -0600

    setup initial build

---
+++ build/precache-manifest.cf5d98c6bddcef59a73b0c74900df25c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "496ef875bbe6bf168acdeff749a2b0b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82da886e7bc53eeefc78",
+    "url": "/static/css/2.913e905a.chunk.css"
+  },
+  {
+    "revision": "1cdd2a3d0cedcc1bdce0",
+    "url": "/static/css/main.0944df2b.chunk.css"
+  },
+  {
+    "revision": "82da886e7bc53eeefc78",
+    "url": "/static/js/2.9bd07cc2.chunk.js"
+  },
+  {
+    "revision": "1a7338b9779c429205a459e109b3d484",
+    "url": "/static/js/2.9bd07cc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cdd2a3d0cedcc1bdce0",
+    "url": "/static/js/main.4063a9ad.chunk.js"
+  },
+  {
+    "revision": "3c367772d9804040cdc2",
+    "url": "/static/js/runtime-main.2b4e80c1.js"
+  },
+  {
+    "revision": "72879e3e58b3ddf07235ca0cfd81a7cf",
+    "url": "/static/media/Epics.72879e3e.png"
+  }
+]);
